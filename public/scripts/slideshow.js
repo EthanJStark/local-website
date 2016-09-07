3fc960b5697375fc9ca5d9fc7795a084f019ffd9
Shaka Lee <shakalee14@gmail.com>
2016-09-07 00:36:35 -0500

    wip function for slideshow

--- public/scripts/slideshow.js
+++ public/scripts/slideshow.js
@@ -1,23 +1,10 @@
-const image1=new Image()
-image1.src="./images/pic1.jpg"
+const movetoNext = () => {
+	const pictures = ['./public/images/pic1.jpg', './public/images/pic2.jpg','./public/images/pic3.jpg']
+//let click  = next picture 
 
-const image2=new Image()
-image2.src="./images/pic2.jpg"
+	for( let index = 0; index < pictures.length; index++){
+		pictures[index] = index 
+	}
 
-const image3=new Image()
-image3.src="./images/pic1.jpg"
-
-
-const step=1
-const slideit = () => {
-//if browser does not support the image object, exit.
-	if (!document.images)
-	return
-		document.images.slide.src=eval("image"+step+".src")
-	if (step<3)
-		step++
-	else
-		step=1
-//call function "slideit()" every 2.5 seconds
-		setTimeout("slideit()",2500)
+	return pictures 
 }
