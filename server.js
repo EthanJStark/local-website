6a82ce857b2b4bdc46c970ce64f9bcad1e29b09c
Ethan Stark <ethanstark@gmail.com>
2016-09-09 12:16:30 -0700

    finished

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/gallery', (request, response) => {
   ]
 
   const exhibits = [
-    '/images/exhibit1.jpg',
+    '/images/exhibit1.png',
     '/images/exhibit2.jpg',
     '/images/exhibit3.jpg',
     '/images/exhibit4.jpg',
